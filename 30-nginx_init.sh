d4572234a597ce55b07e0440369fdd8898cf8f9e
Fe <30653202+fe19880924@users.noreply.github.com>
2021-01-19 10:41:16 +0800

    Update 30-nginx_init.sh

--- 30-nginx_init.sh
+++ 30-nginx_init.sh
@@ -2,4 +2,7 @@ sed -i "s/dest_host/$HOST/" /etc/nginx/nginx.conf
 if [ ! $PORT ];then
   DEST_PORT=8888
 fi
+if [! $ENABLE_SSL ];then
+  sed -i 's/#proxy_ssl/proxy_ssl/' /etc/nginx/nginx.conf
+fi
 sed -i "s/dest_port/$PORT/" /etc/nginx/nginx.conf
